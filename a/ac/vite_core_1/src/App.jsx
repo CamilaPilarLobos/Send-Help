9fe3ce4e4a71fa2bd534478051e1f89f74e16727
Camila Pilar Lobos <camilapilarlobos@gmail.com>
2025-11-04 23:43:46 -0300

    hw

---
+++ a/ac/vite_core_1/src/App.jsx
@@ -0,0 +1,24 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+    <header>
+    </header>
+    <main>
+      <h1> ¡Bienvenido a mi aplicaicon de React! </h1>
+      <h2>Lista de cosas por hacer:</h2>
+      <ol>
+        <li>Aprender con react</li>
+        <li>Practicar con Vite</li>
+        <li>Construir proyectos increibles</li>
+      </ol>
+    </main>
+    <footer>
+    </footer>
+    </>
+  )
+}
+
+export default App
