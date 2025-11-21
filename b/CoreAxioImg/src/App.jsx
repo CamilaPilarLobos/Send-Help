0500f9b2e829b477fd365157cdbaef2aa2985340
Camila Pilar Lobos <camilapilarlobos@gmail.com>
2025-11-20 22:11:47 -0300

    DP

---
+++ b/CoreAxioImg/src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import ExtractData from './assets/components/ExtractData'
+
+function App(){
+
+  return (
+    <>
+    <h1> Random Image of A cat :3</h1>
+    <ExtractData />
+    </>
+  )
+}
+
+export default App
